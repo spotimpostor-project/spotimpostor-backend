276817a5e6f12ddfdc5d8cc5c31ac161bef0e0ca
Luis-Enrique-NR <luis.enrique.nunez.ramos@gmail.com>
2026-01-19 09:12:27 -0500

    chore: add health endpoint

---
+++ spotimpostor/src/main/java/com/spotimpostor/spotimpostor/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.spotimpostor.spotimpostor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class HealthController {
+  @GetMapping("/health")
+  public String healthCheck() {
+    return "OK";
+  }
+}
